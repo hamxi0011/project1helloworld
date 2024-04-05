d654cd542d15e99d5c7df1772d8be96d018262ed
hamxi011 <hamxishah1098@gmail.com>
2024-04-05 03:53:24 -0700

    .

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+console.log("hello worl");
+let n1 = 1;
+let n2 = 2;
+let sum = n1 + n2;
+console.log(sum);
+let name1 = "Hamza";
+console.log(name1);
---
+++ index.ts
@@ -0,0 +1,20 @@
+console.log("hello worl");
+
+
+let n1:number = 1
+let n2:number = 2
+
+let sum:number = n1+n2
+
+console.log(sum)
+
+let name1:string = "Hamza"
+
+console.log(name1)
+
+
+
+let name2:string = "Hamza"
+console.log(`before changes ${name2}`)
+name2:string = "Ahmed"
+console.log(`after changes ${name}`)
